2bdc880fdcd89c3b9856361ca46f798722365e55
pkimclarence@yahoo.com.ph <pkimclarence@yahoo.com.ph>
2016-09-23 11:35:59 +0800

    implement node initialization

--- ANN.py
+++ ANN.py
@@ -1,3 +1,4 @@
+import random
 from MultiNode import Node
 
 
@@ -19,45 +20,60 @@ from MultiNode import Node
 #
 #------------------------
 
-def main():
+global Input1
+global Input2
+global Hidden1
+global Hidden2
+global Hidden3
+global Output
+global trainingFile
+
+
+#initialize the nodes
+#------------------------
+def init():
+
+	global trainingFile
+	global Input1
+	global Input2
+	global Hidden1
+	global Hidden2
+	global Hidden3
+	global Output
+
+	#load training file
+	trainingFile = open('TrainingFiles/tf.ann','a')
 
 	#Input Layers
-	#---------------------
-
 	Input1 = Node(3)
 	Input2 = Node(0)
-	
-	#---------------------
 
 	#Hidden Layers
-	#---------------------
-	
 	Hidden1 = Node(2)
 	Hidden2 = Node(0)
 	
 	Hidden3 = Node(0)
-	#---------------------
 
 	#Output Layer
-	#---------------------
-	
 	Output = Node(1)
-	
-	#---------------------
 
 	#Connect the layers
-	#---------------------
-	
 	Input1.connect(Hidden1,Hidden2,Hidden3)
-	
 	Hidden1.connect(Input1,Input2,None)
 	Hidden2.connect(Input1,Input2,None)
 	Hidden3.connect(Input1,Input2,None)
-
-	
 	Output.connect(Hidden1,Hidden2,Hidden3)
-	#---------------------
+#------------------------
 
+def testANN(input1, input2):
+	print("testing...")
+
+def train():
+	print("training..")
+
+
+def main():
+	init()
 	
-	
+
 main()
--- MultiNode.py
+++ MultiNode.py
@@ -1,4 +1,4 @@
-
+import random
 
 # Create the Node or Neuron
 #	- this Node can only be connected
@@ -9,7 +9,11 @@ class Node(object):
 
 	def __init__(self, data):
 		self.data = data
-		self.weight = 0
+
+		self.weight1 = random.random()
+		self.weight2 = random.random()
+		self.weight3 = random.random()
+
 		self.head1 = None
 		self.head2 = None
 		self.head3 = None
@@ -36,6 +40,19 @@ class Node(object):
 			self.head = self.n3
 			return self.head3
 		return None
+
+	def next(self, n, w):
+		if n==0:
+			self.head1.setWeight(w)
+			return self.head1
+		elif n==1:
+			self.head2.setWeight(w)
+			return self.head2
+		elif n==2:
+			self.head3.setWeight(w)
+			return self.head3
+		return None
+
 
 	def connect(self, N, N2, N3):
 		self.head1 = N
